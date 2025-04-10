1c0c2cf395ba663164ea65fef0572fe3643579d5
Gesa <gesavlop@gmail.com>
2025-04-10 16:14:19 +0700

    mm

--- resources/views/component/navbar.blade.php
+++ resources/views/component/navbar.blade.php
@@ -8,14 +8,6 @@
             <li><a href="/about">About</a></li>
             <li><a href="/menu">Menu</a></li>
         </ul>
-        <div class="nav-buttons">
-            <form action="" method="GET">
-                <button id="btLogin" type="submit">Masuk</button>
-            </form>
-            <form action="" method="GET">
-                <button id="create" type="submit">Daftar</button>
-            </form>
-        </div>
     </div>
     <div class="menu-icon" id="nav-toggle">
         <i class='bx bx-menu'></i>
